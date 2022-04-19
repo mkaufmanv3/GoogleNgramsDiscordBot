be804eccdd937705e492cc183f82440c3d059514
mkaufmanv3 <103878203+mkaufmanv3@users.noreply.github.com>
2022-04-19 17:33:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,10 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # DM handler
+    if not message.guild and message.author != client.user:
+        await message.channel.send("u r gay")
+        return
 
     # Don't listen to self
     if message.author == client.user:
@@ -51,19 +55,10 @@ async def on_message(message):
     #                            #
     #****************************#
     if message.content.startswith('.'):
-        msg = message.content
-        if ',' in msg:
-            msg_split = msg.split(',')
-            search = msg_split[0][1:]
-            if 'avg' in msg_split[1]:
-                avg = True
-            if 'span' in msg_split[1]:
-                span = int(msg_split[1][1:])
-        else:
-            search = msg[1:]
-        print(f"\n>>>  {message.author.name} sent:  '{search}'", end='')
-        try: plot(search)
-        except: return
+        query = message.content
+        print(f"\n>>>  {message.author.name} sent:  '{query}'", end='')
+        try:  plot(query)
+        except Exception as e:  print(f"\n>>>  Error @ 61:  {e}")
         await message.channel.send(file=discord.File('pic.png'))
         print(f"\t--> Completed query in {round(time.perf_counter()-start,3)} seconds\n")
     
@@ -80,16 +75,16 @@ async def on_message(message):
             to_delete = 2
         else:
             to_delete_str = message.content[8:]
-            try: to_delete = int(to_delete_str)*2
-            except: to_delete = 2
+            try:  to_delete = int(to_delete_str)*2
+            except:  to_delete = 2
         if to_delete == 2:
             print(f"\n>>>  {requestor.name} requested deletion of 1 item ({to_delete} messages).")
         else:
             print(f"\n>>>  {requestor.name} requested deletion of {int(to_delete/2)} items ({to_delete} messages).")
 
         if to_delete > 6:
-            await message.channel.send(f"{requestor.name},  I'm sorry,  but I can only delete 3 search histories at a time. To delete more, specify 'all'.")
-            print(f">>>  {requestor.name}'s delete request was denied due to too large a size.")
+            await message.channel.send(f"{requestor.name}, you can only delete 3 search histories at a time. To delete more, specify 'all'.")
+            print(f">>>  {requestor.name}'s delete request was denied due to too large a size ({int(to_delete/2)} items/{to_delete} messages).")
             
         else:
             count = 0
@@ -101,23 +96,21 @@ async def on_message(message):
                     count -= 1
                     await message.delete()
                     print(f"-->  Deleted  '{message.content}'  request message.")
-                elif message.author == client.user and "I'm sorry" in message.content:
+                elif message.author == client.user and "you can only" in message.content:
                     count -= 1
                     await message.delete()
-                    print(f"-->  Deleted an 'I'm sorry' message.")
+                    print(f"-->  Deleted a 'to_delete > 6' warning message.")
                 elif message.author == requestor and message.content.startswith('.'):
                     await message.delete()
                     print(f"-->  Deleted message #{count} of {requested} (query '{message.content}').")
                     to_delete -= 1
-                    if to_delete == 0:
-                        break
+                    if to_delete == 0:  break
                 elif message.author == client.user:
                     await message.delete()
                     print(f"-->  Deleted message #{count} of {requested} (ngram plot).")
                     to_delete -= 1
-                    if to_delete == 0:
-                        break
-                    
+                    if to_delete == 0:  break
+
             print(f"\n>>>  Finished deletion request from {requestor.name} in {round(time.perf_counter()-start,3)} seconds.\n")
     
 
@@ -138,10 +131,10 @@ async def on_message(message):
                 count -= 1
                 await message.delete()
                 print(f"-->  Deleted  '{message.content}'  request message.")
-            elif message.author == client.user and "I'm sorry" in message.content:
+            elif message.author == client.user and "you can only" in message.content:
                 count -= 1
                 await message.delete()
-                print(f"-->  Deleted an 'I'm sorry' message.")
+                print(f"-->  Deleted a 'to_delete > 6' warning message.")
             elif message.author == requestor and message.content.startswith('.'):
                 await message.delete()
                 print(f"-->  Deleted message #{count} (query '{message.content}').")
@@ -150,6 +143,7 @@ async def on_message(message):
                 print(f"-->  Deleted message #{count} (ngram plot).")
 
         print(f"\n>>>  Finished 'all' deletion request from {requestor.name} in {round(time.perf_counter()-start,3)} seconds.\n")
+
 
 
 
