df24a3f68f75997be3e0705181ac40154e2264ef
mkaufmanv3 <103878203+mkaufmanv3@users.noreply.github.com>
2022-04-19 17:33:05 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -25,16 +25,14 @@ from urllib.request import urlopen
 #              year where the global maximum is reached   #
 #                                                         #
 #*********************************************************#
-def normalize(timeseries):
-    nozeros = [x for x in timeseries if x != 0]                       # remove zeros from the timeseries
-    smallest = min(nozeros)                                           # find minimum value in the timeseries
-    norm = [round(value/smallest,5) for value in timeseries]          # normalize the timeseries to the smallest value
-    biggest = max(norm)                                               # find the maximum value in the normalized timeseries
+def normalize(timeseries, start):
+    nozeros = [x for x in timeseries if x != 0]                 # remove zeros from the timeseries
+    smallest = min(nozeros)                                     # find minimum value in the timeseries
+    norm = [round(value/smallest,5) for value in timeseries]    # normalize the timeseries to the smallest value
+    biggest = max(norm)                                         # find the maximum value in the normalized timeseries
     for i in range(len(norm)):
-        if norm[i] == 1:
-            smallest_year = i+1800                                    # find the year in which the smallest value is reached
-        if norm[i] == biggest:
-            biggest_year = i+1800                                     # find the year in which the biggest value is reached
+        if norm[i] == 1:  smallest_year = i+start               # find the year in which the smallest value is reached
+        if norm[i] == biggest:  biggest_year = i+start          # find the year in which the biggest value is reached
     return norm, smallest_year, biggest_year
 
 
@@ -50,12 +48,12 @@ def normalize(timeseries):
 #                                                         #
 #*********************************************************#
 def get(query):
-    url, search, start, end = getURL(query)                           # get ngram url
+    url, search, start, end = getURL(query)                            # get ngram url
     with urlopen(url) as response:
-        datajson = json.loads(response.read())[0]                     # read the json data
-    try: timeseries = datajson['timeseries']                          # extract the timeseries
-    except Exception as e: print(f"\n>>>  Error:  {e}")
-    norm, smallest_year, biggest_year = normalize(timeseries)         # normalize the timeseries
+        datajson = json.loads(response.read())[0]                      # read the json data
+    try:  timeseries = datajson['timeseries']                          # extract the timeseries
+    except Exception as e:  print(f"\n>>>  Error @ f55:  {e}")
+    norm, smallest_year, biggest_year = normalize(timeseries, start)   # normalize the timeseries
     return norm, smallest_year, biggest_year, search, start, end
 
 
@@ -71,46 +69,68 @@ def get(query):
 #*********************************************************#
 def getURL(query, start=1800, end=2019, smoothing=0, **kwargs):
     if ',' in query:
-        split_str = query.split(',')                                  # split query into list
-        if split_str[0][0] != '.':                                    # only accept queries beginning with '.'
-            return
-        else:
-            search = split_str[0][1:]                                 # take everything from '.' to ',' for search string
+        split_str = query.split(',')                        # split the query string by commas
+
+        if split_str[0][0] != '.':  return                  # only accept queries beginning with '.'
+        else:  search = split_str[0][1:]                    # take everything from '.' to ',' for search string
+
         if ' ' in search:
-            search = search.replace(' ', '+')                         # spaces need to be '+'s in URL
-    
+            search = search.replace(' ', '+')               # spaces need to be '+'s in google ngram URL
+
         for i in range(1, len(split_str)):
             if 'start' in split_str[i]:
-                try: start = int(split_str[i].split('=')[1])          # get start year as int
-                except Exception as e: print(f"\n>>>  {e}")
+                if '=' in split_str[i]:                                           # if the start year is specified with '='
+                    try:  start = int(split_str[i].split('=')[1])                   # get start year as an int
+                    except Exception as e:  print(f"\n>>>  Error @ f83:  {e}")
+                elif 'at' in split_str[i]:                                        # if the start year is specified with 'at'
+                    try:  start = int(split_str[i].split('at')[1])                  # get start year as an int
+                    except Exception as e:  print(f"\n>>>  Error @ f86:  {e}")
+
             if 'end' in split_str[i]:
-                try: end = int(split_str[i].split('=')[1])            # get end year as int
-                except Exception as e: print(f"\n>>>  {e}")
+                if '=' in split_str[i]:                                           # if the end year is specified with '='
+                    try:  end = int(split_str[i].split('=')[1])                     # get end year as an int
+                    except Exception as e:  print(f"\n>>>  Error @ f91:  {e}")
+                elif 'at' in split_str[i]:                                        # if the end year is specified with 'at'
+                    try:  end = int(split_str[i].split('at')[1])                    # get end year as an int
+                    except Exception as e:  print(f"\n>>>  Error @ f94:  {e}")
+
             if 'smoothing' in split_str[i]:
-                try: smoothing = int(split_str[i].split('=')[1])      # get smoothing value as int
-                except Exception as e: print(f"\n>>>  {e}")
+                try:  smoothing = int(split_str[i].split('=')[1])                 # get smoothing amount as an int
+                except Exception as e:  print(f"\n>>>  Error @ f98:  {e}")
+    
     else:
-        search = query[1:]                                            # search string is everything after '.' in query
+        search = query[1:]                                  # search string is everything after '.' in query string
         if ' ' in search:
-            search = search.replace(' ', '+')                         # spaces need to be '+'s in URL
+            search = search.replace(' ', '+')               # spaces need to be '+'s in google ngram URL
     
-    if start > 2019 or start < 1500:                                  # start year can only be between 1500 and 2019
-        start = 1800
-    if end > 2019 or end < 1500:                                      # end year can only be between 1500 and 2019
-        end = 2019
-    if start > end:                                                   # start cannot be greater than end
-        start, end = end, start
-    if smoothing < 0:                                                 # smoothing cannot be negative
-        smoothing = -smoothing
+    if start > end:  start, end = end, start                # start year cannot be greater than end year
+    if end > 2019 or end < 1500:  end = 2019                # end year can only be between 1500 and 2019
+    if smoothing < 0:  smoothing = -smoothing               # smoothing amount cannot be a negative number
+    if start > 2019 or start < 1500:  start = 1800          # start year can only be between 1500 and 2019
     
-    url = (
-      'https://books.google.com/ngrams/json?content=' + search +
-      '&year_start=' + str(start) + '&year_end=' + str(end) +
-      '&corpus=26&smoothing=' + str(smoothing) + '&case_insensitive=true'
-    )
+    url = f"https://books.google.com/ngrams/json?content={search}&year_start={start}&year_end={end}&corpus=26&smoothing={smoothing}&case_insensitive=true"
     return url, search, start, end
 
 
+
+#*********************************************************#
+#                                                         #
+#   Step-size calculator                                  #
+#     Inputs:  start year, end year                       #
+#     Output:  an integer step-size,                      #
+#              such that 7 < x-axis tick count < 13       #
+#                                                         #
+#*********************************************************#
+def stepsize(start, end):
+    if   end-start < 30:   return 3
+    elif end-start > 29    and end-start <=  59:  return 5
+    elif end-start > 59    and end-start <= 104:  return 10
+    elif end-start > 104   and end-start <= 139:  return 15
+    elif end-start > 139   and end-start <  175:  return 20
+    elif end-start >= 175  and end-start <  324:  return 25
+    elif end-start >= 324  and end-start <  389:  return 30
+    elif end-start >= 389  and end-start <  454:  return 35
+    elif end-start >= 454: return 40
 
 
 
@@ -121,21 +141,24 @@ def getURL(query, start=1800, end=2019, smoothing=0, **kwargs):
 #     Output: png image file (saves locally for main)     #
 #                                                         #
 #*********************************************************#
-def plot(query, avg=False, span=2):
-    x = [i+start for i in range(len(timeseries))]
+def plot(query):
     timeseries, smallest_year, biggest_year, search, start, end = get(query)
-    print(f"\n\n[plot function]>>>  Searching for {search} from {start} to {end}\n\n")
+    print(f"\n\n[plot function]>>>  Searching for {search} from {start} to {end}", end='')
+    print(f" with smallest_year = {smallest_year}, and bigggest_year = {biggest_year}\n\n")
 
-    pyplot.tight_layout()
+    step = stepsize(start,end)
     pyplot.style.use('ggplot')
     pyplot.rcParams['figure.figsize'] = (17, 10)
+    pyplot.tight_layout()
     pyplot.subplots_adjust(right=0.93, bottom=0.15)
-    pyplot.yticks(font=Path('font/InconsolataRegular.ttf'), fontsize=30, color='white')
-    pyplot.xticks(range(start, end, 25), font=Path('font/InconsolataRegular.ttf'), fontsize=30, color='white')
+
+    pyplot.title(f'"{search.replace("+", " ")}"', font=Path('font/JetBrainsMonoExtraBold.ttf'), fontsize = 35, pad = 30, color='white')
     pyplot.xlabel('year', font=Path('font/JetBrainsMonoMediumItalic.ttf'), fontsize=33, labelpad=41, color='white')
     pyplot.ylabel('frequency', font=Path('font/JetBrainsMonoMediumItalic.ttf'), fontsize=30, labelpad=38, color='white')
-    pyplot.title('"'+str(search)+'"', font=Path('font/JetBrainsMonoExtraBold.ttf'), fontsize = 35, pad = 30, color='white')
+    pyplot.xticks(range(start, end, step), font=Path('font/InconsolataRegular.ttf'), fontsize=30, color='white')
+    pyplot.yticks(font=Path('font/InconsolataRegular.ttf'), fontsize=30, color='white')
 
+    x = [i+start for i in range(len(timeseries))]
     pyplot.plot(x,timeseries)
     pyplot.axvline(x=biggest_year, color='#BBBBBB', linestyle='--')
     pyplot.axvline(x=smallest_year, color='#BBBBBB', linestyle='--')
